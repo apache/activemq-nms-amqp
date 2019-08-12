a225597349909c212b48ca3675886b066d3301ba
Havret <h4vret@gmail.com>
2019-08-12 11:49:22 +0200

    Batchable flag set to false for sync sends workaround

--- src/NMS.AMQP/Provider/Amqp/AmqpProducer.cs
+++ src/NMS.AMQP/Provider/Amqp/AmqpProducer.cs
@@ -121,7 +121,13 @@ namespace Apache.NMS.AMQP.Provider.Amqp
                     if (envelope.SendAsync)
                         SendAsync(message, transactionalState);
                     else
-                        SendSync(message, transactionalState);
+                    {
+                        // TODO: Should be unified after https://github.com/Azure/amqpnetlite/pull/374 is sorted out
+                        if (transactionalState != null)
+                            SendSync(message, transactionalState);
+                        else
+                            senderLink.Send(message, TimeSpan.FromMilliseconds(session.Connection.Provider.SendTimeout));
+                    }
                 }
                 catch (TimeoutException tex)
                 {
